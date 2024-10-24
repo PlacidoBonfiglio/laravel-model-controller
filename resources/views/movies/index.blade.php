e31c2907f16ab0982a30572a89f2caf29c32d1f5
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-10-24 18:37:07 +0200

    fixed movies title

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -9,7 +9,7 @@
             @foreach ( $movies as $movie )
                 <div class="col-3 text-center mb-5">
                     <article>
-                        <h4 class="text-white">TITLE: {{ $movie->title }}</h4>
+                        <h4 class="text-white">{{ $movie->title }}</h4>
                         <h5 class="text-white">Original title: {{ $movie->original_title }}</h5>
                         <p class="text-white">Nationality: {{ $movie->nationality }}</p>
                         <p class="text-white">Date: {{ $movie->date }}</p>
