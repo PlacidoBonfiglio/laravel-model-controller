9896743610c556f6b81645956042179324c7f1f3
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-10-24 18:43:33 +0200

    end

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,5 +3,10 @@
 @section("page-title", "Laravel Model Controller")
 
 @section("main-content")
-    <h1 class="text-center">/movies</h1>
+    <a href="/movies">
+        <h1 class="text-center mt-5">
+            Cliccami per vedere la lista dei film!
+        </h1>
+    </a>
+
 @endsection
